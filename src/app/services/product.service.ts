302cebeb6b926e7a9c152744b4427925f81b54aa
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-12 13:06:29 +0100

    set the product service observable

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private baseUrl = 'http://localhost:3000/api/products';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.baseUrl);
+  }
 }
