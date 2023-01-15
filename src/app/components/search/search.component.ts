06021bf4f5a8cc7e28cf338ae09a919c2e024235
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-15 16:45:33 +0100

    define the doSearch function

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  constructor(private router: Router) {}
 
+  doSearch(value: string) {
+    this.router.navigateByUrl(`/search/${value}`);
+  }
+
+  ngOnInit(): void {}
 }
