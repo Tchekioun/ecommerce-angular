9cb0ce95a84dbb49b0bd73ef2a33b7145d8b4a5f
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-15 16:05:48 +0100

    add get all categoies service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Category } from '../common/category';
 import { Product } from '../common/product';
 
 @Injectable({
@@ -8,6 +9,7 @@ import { Product } from '../common/product';
 })
 export class ProductService {
   private baseUrl = 'http://localhost:3000/products';
+  private categoryUrl = 'http://localhost:3000/categories';
 
   constructor(private httpClient: HttpClient) {}
 
@@ -15,5 +17,9 @@ export class ProductService {
     return this.httpClient.get<Product[]>(
       `${this.baseUrl}/search/findByCategoryId?id=${id}`
     );
+  }
+
+  getCategories(): Observable<Category[]> {
+    return this.httpClient.get<Category[]>(this.categoryUrl);
   }
 }
