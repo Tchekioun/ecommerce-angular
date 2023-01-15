770a1c37690f85aa5aae6208ff2f51ba361ff0c4
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-15 12:27:47 +0100

    updated service to filter products by cateogry

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -9,14 +10,28 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
-  constructor(private productService: ProductService) {}
+  currentCateogryId!: number;
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit() {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe((data) => {
-      this.products = data;
-    });
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    if (hasCategoryId) {
+      this.currentCateogryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      this.currentCateogryId = 1;
+    }
+    this.productService
+      .getProductList(this.currentCateogryId)
+      .subscribe((data) => {
+        this.products = data;
+      });
   }
 }
