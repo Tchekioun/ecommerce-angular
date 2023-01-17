e7160807fc2240b4ef984191106166d48098536b
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-17 15:55:22 +0100

    call the listproduct in case of search

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -77,6 +77,6 @@ export class ProductListComponent implements OnInit {
 
   updatePageSize(value: number) {
     this.thePageSize = value;
-    this.handleListProducts();
+    this.listProducts();
   }
 }
