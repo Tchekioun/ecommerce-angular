1d8e4bb000f096203f73882ed5fd759294bbdd28
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-18 16:32:14 +0100

    FIX: spelling error

--- src/app/common/cart-item.ts
+++ src/app/common/cart-item.ts
@@ -3,15 +3,17 @@ import { Product } from './product';
 export class CartItem {
   id: number;
   name: string;
+  description: string;
   image_url: string;
   unit_price: number;
-  quanitity: number;
+  quantity: number;
 
   constructor(product: Product) {
     this.id = product.id;
     this.name = product.name;
+    this.description = product.description;
     this.image_url = product.image_url;
     this.unit_price = product.unit_price;
-    this.quanitity = 1;
+    this.quantity = 1;
   }
 }
