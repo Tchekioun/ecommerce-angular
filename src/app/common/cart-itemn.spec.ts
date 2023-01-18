308a30685f46aa95194271d62a0677402a8ec9fb
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-18 11:56:49 +0100

    add carItem class

---
+++ src/app/common/cart-itemn.spec.ts
@@ -0,0 +1,7 @@
+import { CartItemn } from './cart-itemn';
+
+describe('CartItemn', () => {
+  it('should create an instance', () => {
+    expect(new CartItemn()).toBeTruthy();
+  });
+});
---
+++ src/app/common/cart-itemn.ts
@@ -0,0 +1,17 @@
+import { Product } from './product';
+
+export class CartItemn {
+  id: number;
+  name: string;
+  image_url: string;
+  unit_price: number;
+  quanitity: number;
+
+  constructor(product: Product) {
+    this.id = product.id;
+    this.name = product.name;
+    this.image_url = product.image_url;
+    this.unit_price = product.unit_price;
+    this.quanitity = 1;
+  }
+}
