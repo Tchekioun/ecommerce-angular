21fb5fe6432c02b0690c53a9db4282194bde0502
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-18 16:38:36 +0100

    Add listCartDetails

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
   templateUrl: './cart-details.component.html',
-  styleUrls: ['./cart-details.component.css']
+  styleUrls: ['./cart-details.component.css'],
 })
-export class CartDetailsComponent {
+export class CartDetailsComponent implements OnInit {
+  constructor(private cartService: CartService) {}
 
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+    this.cartItems = this.cartService.cartItems;
+    this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
+    this.cartService.totalQuantity.subscribe(
+      (data) => (this.totalQuantity = data)
+    );
+    this.cartService.computeCartTotals();
+  }
 }
