686d527e5765ca365a0d2d44bb702adb7cc99dde
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-18 15:29:49 +0100

    add to Cart from detail view

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -13,7 +15,8 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private productService: ProductService
+    private productService: ProductService,
+    private cartService: CartService
   ) {}
 
   ngOnInit(): void {
@@ -27,5 +30,10 @@ export class ProductDetailsComponent implements OnInit {
     this.productService.getProductDetails(theProductId).subscribe((data) => {
       this.product = data;
     });
+  }
+
+  addToCart(product: Product) {
+    const theCartItem = new CartItem(product);
+    this.cartService.addToCart(theCartItem);
   }
 }
