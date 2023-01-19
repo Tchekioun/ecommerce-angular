f1849d9b97b5f96be21be71b09cdf5ca10c52ad7
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-19 16:39:50 +0100

    define get creditcard month fn

--- src/app/services/myform.service.ts
+++ src/app/services/myform.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MyformService {
+  constructor() {}
 
-  constructor() { }
+  getCrediCardMonths(startMonth: number): Observable<number[]> {
+    let data: number[] = [];
+    for (let theMonth = startMonth; theMonth <= 12; theMonth++) {
+      data.push(theMonth);
+    }
+    return of(data);
+  }
 }
