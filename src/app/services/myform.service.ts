61ea71439b48cab046519e5ed9ab3270024082c1
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-19 16:44:28 +0100

    define get 10 year ahead fn

--- src/app/services/myform.service.ts
+++ src/app/services/myform.service.ts
@@ -14,4 +14,14 @@ export class MyformService {
     }
     return of(data);
   }
+
+  getCreditCardYears(): Observable<number[]> {
+    let data: number[] = [];
+    const startYear: number = new Date().getFullYear();
+    const endYear: number = startYear + 10;
+    for (let theYear = startYear; theYear <= endYear; theYear++) {
+      data.push(theYear);
+    }
+    return of(data);
+  }
 }
