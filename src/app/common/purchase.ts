eca1bb27819a4a67ccafe65af51d8510b6c4c982
Abderraouf <abdelraouffinch@gmail.com>
2023-01-24 21:00:03 +0100

    purchase class

--- src/app/common/purchase.ts
+++ src/app/common/purchase.ts
@@ -1,2 +1,12 @@
+import { Address } from './address';
+import { Customer } from './customer';
+import { Order } from './order';
+import { OrderItem } from './order-item';
+
 export class Purchase {
+  customer!: Customer;
+  shipping_address!: Address;
+  billing_address!: Address;
+  order!: Order;
+  order_item!: OrderItem;
 }
