2cd33f3c85627c98c549ea81609db7853d18bdfb
Abderraouf <abdelraouffinch@gmail.com>
2023-01-24 20:59:45 +0100

    order-item class

--- src/app/common/order-item.ts
+++ src/app/common/order-item.ts
@@ -1,2 +1,15 @@
+import { CartItem } from './cart-item';
+
 export class OrderItem {
+  image_url!: string;
+  unit_price!: number;
+  quantity!: number;
+  product_id: number;
+
+  constructor(cartItem: CartItem) {
+    this.image_url = cartItem.image_url;
+    this.product_id = cartItem.id;
+    this.quantity = cartItem.quantity;
+    this.unit_price = cartItem.unit_price;
+  }
 }
