6f1b7810bb5c8096b4da9647ce959926a9ca0bd5
Abderraouf <abdelraouffinch@gmail.com>
2023-01-26 20:09:26 +0100

    Add: allowedCommonJsDependencies

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,7 +2,9 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { OKTA_AUTH } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
 import OktaSignIn from '@okta/okta-signin-widget';
+
 import myAppConfig from '../../config/my-app-config';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,6 +12,7 @@ import myAppConfig from '../../config/my-app-config';
 })
 export class LoginComponent implements OnInit {
   oktaSignin: any;
+
   constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
     this.oktaSignin = new OktaSignIn({
       logo: 'assets/images/logo.png',
@@ -26,17 +29,18 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.oktaSignin.remove();
+
     this.oktaSignin.renderEl(
       {
         el: '#okta-sign-in-widget',
-      }, //same as component.html id div tag
+      }, // this name should be same as div tag id in login.component.html
       (response: any) => {
         if (response.status === 'SUCCESS') {
           this.oktaAuth.signInWithRedirect();
         }
       },
       (error: any) => {
-        throw error;
+        console.log(`THERE IS AN ERROR:${error}`);
       }
     );
   }
--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,6 +1,6 @@
 export default {
   oidc: {
-    clientId: '0oa83u0z0c7AnnLye5d7',
+    clientId: '0oa8415d8mVhXkmfc5d7',
     issuer: 'https://dev-09618689.okta.com/oauth2/default',
     redirectUri: 'http://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
