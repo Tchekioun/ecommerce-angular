7cb5a6c667a4e1eb55cf1932b18a39187c05e5a7
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2023-01-26 16:26:28 +0100

    FEATT: login status

---
+++ src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,44 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
+import OktaAuth from '@okta/okta-auth-js';
+
+@Component({
+  selector: 'app-login-status',
+  templateUrl: './login-status.component.html',
+  styleUrls: ['./login-status.component.css'],
+})
+export class LoginStatusComponent implements OnInit {
+  isAuthenticated: boolean = false;
+  userFullname: string = '';
+
+  constructor(
+    private oktaAuthService: OktaAuthStateService,
+    @Inject(OKTA_AUTH) private oktaAuth: OktaAuth
+  ) {}
+
+  ngOnInit(): void {
+    //subscribe to authentication state changes
+
+    this.oktaAuthService.authState$.subscribe((results) => {
+      this.isAuthenticated = results.isAuthenticated!;
+      this.getUserDetails();
+    });
+  }
+  getUserDetails() {
+    if (this.isAuthenticated) {
+      //Fetch the logged in user details (user's claim)
+
+      //user full name is exposed as a property name
+
+      this.oktaAuth.getUser().then((res) => {
+        this.userFullname = res.name as string;
+      });
+    }
+  }
+
+  logout() {
+    //Terminate the session with Okta and remove the current tokens
+
+    this.oktaAuth.signOut();
+  }
+}
