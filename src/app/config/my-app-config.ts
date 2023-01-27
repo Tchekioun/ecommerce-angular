1140e1b3ee02f70bb382f190ed7abb83746c60cd
Abderraouf <abdelraouffinch@gmail.com>
2023-01-28 00:50:53 +0100

    FEAT: add ssl certafication and securing

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oa83u0z0c7AnnLye5d7',
     issuer: 'https://dev-09618689.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
